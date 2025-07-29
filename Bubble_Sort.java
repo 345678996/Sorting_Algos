7f5e5f5a4feacf078ce1c39ecef95661dfb880b6
Kanav Rohatgi <kanavr@gmail.com>
2025-07-29 14:54:27 +0530

    Optimized Bubble sort

--- Bubble_Sort.java
+++ Bubble_Sort.java
@@ -21,13 +21,16 @@ public class Bubble_Sort {
     }
     public static void BubbleSort(int[] arr, int n) {
         for (int i = n-1; i > 0; i--) {
+            int didSwap = 0;
             for (int j = 0; j < i; j++) {
                 if (arr[j] > arr[j+1]) {
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    didSwap = 1;
                 }
             }
+            if (didSwap == 0) break;
         }
     }
 }
